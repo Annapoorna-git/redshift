544a349cbf8048cd9f8a61cd1161be4ce61e5305
Annapoorna-git <111627448+Annapoorna-git@users.noreply.github.com>
2023-03-28 11:29:52 +0530

    Update redshift.sql

--- redshift.sql
+++ redshift.sql
@@ -1,7 +1,11 @@
+-- Create a schema
 CREATE SCHEMA my_schema;
 
+-- Create a table
 CREATE TABLE my_schema.my_table (
-   id INT PRIMARY KEY,
-   name VARCHAR(50),
-   age INT
+   id INT NOT NULL,
+   name VARCHAR(50) NOT NULL,
+   age INT,
+   gender VARCHAR(10),
+   PRIMARY KEY (id)
 );
