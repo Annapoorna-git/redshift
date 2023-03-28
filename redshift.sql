445191042824cf72aa74d5c07af3d92ddc5bf559
Annapoorna-git <111627448+Annapoorna-git@users.noreply.github.com>
2023-03-28 14:59:58 +0530

    Update redshift.sql

--- redshift.sql
+++ redshift.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA sales_table;
+CREATE SCHEMA sir_table;
 
-CREATE TABLE sales_table; myusers1 (
+CREATE TABLE sir_table; mytable (
  user_id INT PRIMARY KEY,
  user_first_name VARCHAR (30),
   user_last_name VARCHAR (30)
